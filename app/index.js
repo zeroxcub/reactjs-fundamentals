cef038b1b5836e1202312d594c415e3ae86cf41e
zeroxcub <zeroxcub@gmail.com>
2016-12-26 18:24:38 +0300

    First Hello World React component

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-var app = document.getElementById("app").innerHTML = "Hello"
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  render: function() {
+    return (
+      <div> Hello Friends </div>
+    );
+  }
+});
+
+ReactDOM.render(<Hello/>, document.getElementById("app"));
