cf18824e1c586ea1553e62a28d9cbb6c076caa02
zeroxcub <zeroxcub@gmail.com>
2017-01-06 11:45:17 +0300

    feat: separate container and presentational components

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,11 +7,14 @@ var hashHistory = ReactRouter.hashHistory;
 
 var Main = require('../component/Main');
 var Home = require('../component/Home');
+var PromptContainer = require('../container/PromptContainer')
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <IndexRoute component={Home}/>
+      <IndexRoute component={Home} />
+      <Route path='playerOne' header="Player One" component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header="Player Two" component={PromptContainer} /> 
     </Route>
   </Router>
 );
